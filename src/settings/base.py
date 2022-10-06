26fac958b2d3a32a6b0420415a5b65ee6ec29232
n-pradip <64666568+n-pradip@users.noreply.github.com>
2022-10-07 00:12:17 +0545

    fix: update corsheaders middleware in settings

--- src/settings/base.py
+++ src/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = INBUILT_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
